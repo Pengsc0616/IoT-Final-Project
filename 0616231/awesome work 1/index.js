a6d404dac44fbb4c304c4f1b2ae46dd964374d44
Pengsc0616 <st282238@gmail.com>
2020-06-06 13:32:06 +0800

    temp change platform length

--- 0616231/awesome work 1/index.js
+++ 0616231/awesome work 1/index.js
@@ -20,16 +20,17 @@ var status = 'running';
 function preload () {
 
     game.load.baseURL = 'https://wacamoto.github.io/NS-Shaft-Tutorial/assets/';
+    //game.load.baseURL = 'https://Pengsc0616.'
     game.load.crossOrigin = 'anonymous';
-    game.load.spritesheet('player', 'player.png', 32, 32);
-    game.load.image('wall', 'wall.png');
-    game.load.image('ceiling', 'ceiling.png');
-    game.load.image('normal', 'normal.png');
-    game.load.image('nails', 'nails.png');
-    game.load.spritesheet('conveyorRight', 'conveyor_right.png', 96, 16);
-    game.load.spritesheet('conveyorLeft', 'conveyor_left.png', 96, 16);
-    game.load.spritesheet('trampoline', 'trampoline.png', 96, 22);
-    game.load.spritesheet('fake', 'fake.png', 96, 36);
+    game.load.spritesheet('player', 'assets/player.png', 32, 32);
+    game.load.image('wall', 'assets/wall.png');
+    game.load.image('ceiling', 'assets/ceiling.png');
+    game.load.image('normal', 'assets/normal.png');
+    game.load.image('nails', 'assets/nails.png');
+    game.load.spritesheet('conveyorRight', 'assets/conveyor_right.png', 96, 16);
+    game.load.spritesheet('conveyorLeft', 'assets/conveyor_left.png', 96, 16);
+    game.load.spritesheet('trampoline', 'assets/trampoline.png', 96, 22);
+    game.load.spritesheet('fake', 'assets/fake.png', 96, 36);
 }
 
 function create () {
@@ -83,7 +84,7 @@ function createBounders () {
 
 var lastTime = 0;
 function createPlatforms () {
-    if(game.time.now > lastTime + 1200) { //600) { //change here
+    if(game.time.now > lastTime + 800) { //600) { //change here
         lastTime = game.time.now;
         createOnePlatform();
         distance += 1;
@@ -160,7 +161,16 @@ function updatePlayer () {
 //    } else {
 //        player.body.velocity.x = 0;
 //    }
-	player.body.velocity.x = vel_x;
+    //player.body.velocity.x = vel_x;
+    if(vel_x > 0){
+        player.body.velocity.x = 100;
+    }
+    else if(vel_x < 0){
+        player.body.velocity.x = -100;
+    }
+    else{
+        player.body.velocity.x = 0;
+    }
     setPlayerAnimate(player);
 }
 
@@ -191,7 +201,7 @@ function setPlayerAnimate(player) {
 function updatePlatforms () {
     for(var i=0; i<platforms.length; i++) {
         var platform = platforms[i];
-        platform.body.position.y -= 1; //change here
+        platform.body.position.y -= 0.5; //change here
         if(platform.body.position.y <= -20) {
             platform.destroy();
             platforms.splice(i, 1);
